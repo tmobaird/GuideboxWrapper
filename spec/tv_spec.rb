bbe56a6ce7be1178c61973efc4b36d17065ad7ff
Thomas Baird <tbaird@mail.umw.edu>
2015-12-14 01:43:11 +0000

    Adding specs for Tv wrapper

--- spec/tv_spec.rb
+++ spec/tv_spec.rb
@@ -3,28 +3,33 @@ require 'spec_helper'
 describe GuideboxWrapper do
 	describe "GuideboxTv" do
 		let(:guidebox) { GuideboxWrapper::Tv.new(CredentialsHelper.key, "all") }
-		describe "#search_for" do
-			it "returns entourage show" do
-				expect(guidebox.show_information("entourage")["id"]).to eq(6085)
-			end
+		it "returns entourage show" do
+			expect(guidebox.show_information("entourage")["id"]).to eq(6085)
+		end
+		describe "#status" do
 			it "returns ended" do
 				expect(guidebox.status("entourage")).to eq("Ended")
 			end
-			describe "#seasons" do
-				it "returns 8 seasons" do
-					expect(guidebox.seasons("entourage").size).to eq(8)
-				end
+		end
+		describe "#seasons" do
+			it "returns 8 seasons" do
+				expect(guidebox.seasons("entourage").size).to eq(8)
 			end
-			describe "#cast" do
-				it "has value Jeremy Piven" do
-					expect_cast = guidebox.cast("entourage").find { |h| h["name"] == "Jeremy Piven" }
-					expect(expect_cast["name"]).to eq("Jeremy Piven")
-				end
+		end
+		describe "#cast" do
+			it "has value Jeremy Piven" do
+				expect_cast = guidebox.cast("entourage").find { |h| h["name"] == "Jeremy Piven" }
+				expect(expect_cast["name"]).to eq("Jeremy Piven")
 			end
-			describe "#first_aired" do
-				it "returns 2004-07-18" do
-					expect(guidebox.first_aired("entourage")).to eq("2004-07-18")
-				end
+		end
+		describe "#type" do
+			it "returns television" do
+				expect(guidebox.type("entourage")).to eq("television")
+			end
+		end
+		describe "#first_aired" do
+			it "returns 2004-07-18" do
+				expect(guidebox.first_aired("entourage")).to eq("2004-07-18")
 			end
 		end
 	end
